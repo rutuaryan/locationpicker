321a41f1dba39b6dbc3463948a1cc4922c0f9998
rutuaryan <aryanrajpure05@gmail.com>
2026-06-29 03:39:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+console.log("Script started");
 
+mapboxgl.accessToken =
+'pk.eyJ1IjoiZGFzZWluaG9tZWNzIiwiYSI6ImNtcXl3d3R0MjAwYXoycXF2MTNqcjFqaWIifQ.a77W_SYHwiw1Ng4d01Ma9Q';
+
+console.log("Token set");
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [73.8567,18.5204],
+    zoom:15
+});
+
+console.log("Map object created");
+
+map.on("load", () => {
+    console.log("Map loaded");
+});
